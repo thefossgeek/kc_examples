90fdbdfe5a04b82a20f50966c7280ab0dd9dcb6d
Mohan L <thefossgeek@gmail.com>
2014-12-12 00:24:51 +0530

    Script to List all Tenants

---
+++ tenants_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from keystoneclient.v2_0 import client
+import json
+from keystoneclient import utils
+
+keystone = client.Client(username="admin",
+                         password="admin",
+                         tenant_name="admin",
+                         auth_url="http://10.209.224.53:5000/v2.0"
+						)
+tenants_list=keystone.tenants.list()
+print tenants_list
+utils.print_list(tenants_list, ['id', 'name', 'enabled'], order_by='name')
