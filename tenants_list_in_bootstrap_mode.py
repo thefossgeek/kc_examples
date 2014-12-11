32c765e63f97d19671e45bc573f0fcca6f925f76
Mohan L <thefossgeek@gmail.com>
2014-12-12 00:25:54 +0530

    code which list all tenants in bootstrap mode

---
+++ tenants_list_in_bootstrap_mode.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from keystoneclient.v2_0 import client
+from keystoneclient import utils
+
+keystone = client.Client(token="token",
+                         endpoint="http://10.209.224.53:35357/v2.0"
+						)
+tenants = keystone.tenants.list()
+utils.print_list(tenants, ['id', 'name', 'enabled'], order_by='name')
