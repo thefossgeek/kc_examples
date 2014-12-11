781effe77d5bf82ba4c100bf6b5df24d7acaf8e9
Mohan L <thefossgeek@gmail.com>
2014-12-12 00:26:53 +0530

    code example which list all users

---
+++ list_users.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from keystoneclient.v2_0 import client
+from keystoneclient import utils
+
+keystone = client.Client(username="admin",
+                         password="admin",
+                         tenant_name="admin",
+                         auth_url="http://10.209.224.53:5000/v2.0"
+                        )
+users = keystone.users.list()
+utils.print_list(users, ['id', 'name', 'enabled','email'], order_by='name')
